397a0b0d9447362e890e8837d961816b9bf8bb5c
爽爽同学 <1306822660@qq.com>
2023-02-27 20:43:48 +0800

    项目后端代码

---
+++ activity_api/src/main/java/com/ahnu/activity_api/ActivityApiApplication.java
@@ -0,0 +1,13 @@
+package com.ahnu.activity_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityApiApplication.class, args);
+    }
+
+}
